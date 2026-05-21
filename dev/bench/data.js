e1e8046364e3b58a1e00b6c7675cf017630935be
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 07:42:40 +0000

    add Benchmark (cargo) benchmark result for 720ff3a095a9d3f46836f26b02cabe3a261f8fb5

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779349360850,
+  "repoUrl": "https://github.com/saptreekly/Algo-Trader",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jweekly201@gmail.com",
+            "name": "saptreekly",
+            "username": "saptreekly"
+          },
+          "committer": {
+            "email": "jweekly201@gmail.com",
+            "name": "saptreekly",
+            "username": "saptreekly"
+          },
+          "distinct": true,
+          "id": "720ff3a095a9d3f46836f26b02cabe3a261f8fb5",
+          "message": "fix: Correct indentation for cash field in Account struct",
+          "timestamp": "2026-05-21T19:41:52+12:00",
+          "tree_id": "1f88918f63420993a887bdd13993b4f4d8fcdadb",
+          "url": "https://github.com/saptreekly/Algo-Trader/commit/720ff3a095a9d3f46836f26b02cabe3a261f8fb5"
+        },
+        "date": 1779349360837,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "on_tick_performance",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
