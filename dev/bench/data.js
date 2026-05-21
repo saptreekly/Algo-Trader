649079d556f5f9cc8788e78ae6b68c40ba36c0c5
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 07:58:57 +0000

    add Benchmark (cargo) benchmark result for 5f59e33b025c45a38875a39508c69fd2fa603190

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779349360850,
+  "lastUpdate": 1779350337966,
   "repoUrl": "https://github.com/saptreekly/Algo-Trader",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/saptreekly/Algo-Trader/commit/720ff3a095a9d3f46836f26b02cabe3a261f8fb5"
         },
         "date": 1779349360837,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "on_tick_performance",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jweekly201@gmail.com",
+            "name": "saptreekly",
+            "username": "saptreekly"
+          },
+          "committer": {
+            "email": "jweekly201@gmail.com",
+            "name": "saptreekly",
+            "username": "saptreekly"
+          },
+          "distinct": true,
+          "id": "5f59e33b025c45a38875a39508c69fd2fa603190",
+          "message": "feat: Add CSV and Chrono dependencies; implement backtest and data download functionality",
+          "timestamp": "2026-05-21T19:58:02+12:00",
+          "tree_id": "57a10668c5b56caa0261b5052fd8da7504ce70b9",
+          "url": "https://github.com/saptreekly/Algo-Trader/commit/5f59e33b025c45a38875a39508c69fd2fa603190"
+        },
+        "date": 1779350337951,
         "tool": "cargo",
         "benches": [
           {
